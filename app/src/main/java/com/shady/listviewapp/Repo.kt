223da16248a67eceaff1f258594b291922e93287
HP <Abdulaziz.Alhothali.jed.k1@tuwaiq.edu.sa>
2021-11-12 19:22:07 +0300

    HW!!

---
+++ app/src/main/java/com/shady/listviewapp/Repo.kt
@@ -0,0 +1,38 @@
+package com.shady.listviewapp
+
+
+import android.content.Context
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+
+class Repo(context: Context) {
+
+    val cntx=context
+    private val appDB =AppDataBase.getAppDataBase(context)!!
+
+
+
+    suspend fun getAllLangs():List<DB> = withContext(Dispatchers.IO) {
+            appDB.languageDao.getAllLangs()
+        }
+
+
+    suspend fun fillDB()= withContext(Dispatchers.IO){
+
+            val dataDB = appDB.languageDao.getAllLangs()
+        val langList:List<String> = cntx.resources.getStringArray(R.array.array_Language).toList()
+
+        if (dataDB.isEmpty()) {
+            for(i in 1..langList.size-1){
+
+               // val lang =l(i, langList[i])
+                val languageList =DB(langList[i])
+
+              //  context.resources.getStringArray(R.array.array_technology).forEach {
+                   appDB.languageDao.insert(languageList)
+                }
+
+            }}
+
+}
